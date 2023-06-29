b33c30394cd1d161a6333f19384ce7d4bc2cb26f
Nikhil Sarwara <tymnec@gmail.com>
2023-06-29 11:46:14 +0530

    creating a bot

--- main.go
+++ main.go
@@ -11,4 +11,6 @@ import (
 func main(){
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6177862970372-6198505808912-lkopW0bwLCMInXoPksoxKSP4")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A0657U125B6-6175406421523-89c0894f26985bf451479ffa2d538adee03a9f45da05edd1a32e86c501f538fa")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 }
