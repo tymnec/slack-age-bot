bf2a1f0d9421f45211eadb1d1c68744e8b059c3c
Nikhil Sarwara <tymnec@gmail.com>
2023-06-29 11:50:39 +0530

    error check

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
-package main 
+package main
 
 import (
-	"fmt"
-	"log"
 	"context"
-	"os"
+	"fmt"
 	"github.com/shomali11/slacker"
+	"log"
+	"os"
 )
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-	for event := range analyticsChannel{
+	for event := range analyticsChannel {
 		fmt.Println("Command Events")
 		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
@@ -19,7 +19,7 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	}
 }
 
-func main(){
+func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6177862970372-6198505808912-lkopW0bwLCMInXoPksoxKSP4")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A0657U125B6-6175406421523-89c0894f26985bf451479ffa2d538adee03a9f45da05edd1a32e86c501f538fa")
 
@@ -30,5 +30,8 @@ func main(){
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
