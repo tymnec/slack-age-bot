70eddc1009d9d4dd8694450d727fc1d24ebe5b26
Nikhil Sarwara <tymnec@gmail.com>
2023-06-29 11:48:59 +0530

    define print command events function

--- main.go
+++ main.go
@@ -8,9 +8,22 @@ import (
 	"github.com/shomali11/slacker"
 )
 
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel{
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
 func main(){
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6177862970372-6198505808912-lkopW0bwLCMInXoPksoxKSP4")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A0657U125B6-6175406421523-89c0894f26985bf451479ffa2d538adee03a9f45da05edd1a32e86c501f538fa")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
 }
